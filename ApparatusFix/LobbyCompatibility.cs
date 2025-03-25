c5c45f0504a680cc92e75687e00975f0f9f48911
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-03-25 19:13:28 +0100

    code improvements to bring it in line with other projects

---
+++ ApparatusFix/LobbyCompatibility.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ApparatusFix
+{
+  internal class LobbyCompatibilityCompatibility
+  {
+    public static void Init()
+    {
+      Plugin.logger.LogWarning("LobbyCompatibility detected, registering plugin with LobbyCompatibility.");
+
+      Version pluginVersion = Version.Parse(PluginInfo.PLUGIN_VERSION);
+
+      LobbyCompatibility.Features.PluginHelper.RegisterPlugin(
+        PluginInfo.PLUGIN_GUID,
+        pluginVersion,
+        LobbyCompatibility.Enums.CompatibilityLevel.ClientOptional,
+        LobbyCompatibility.Enums.VersionStrictness.None
+      );
+    }
+  }
+}
--- ApparatusFix/Plugin.cs
+++ ApparatusFix/Plugin.cs
@@ -8,78 +8,65 @@ using UnityEngine;
 
 namespace ApparatusFix
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+  [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+  public class Plugin : BaseUnityPlugin
+  {
+    internal static ManualLogSource logger;
+    internal static Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+
+    private void Awake()
     {
-        internal static ManualLogSource logger;
-        internal static Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+      logger = Logger;
+      harmony.PatchAll();
 
-        private void Awake()
+      // Plugin startup logic
+      Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+  }
+
+  [HarmonyPatch(typeof(PlayerControllerB))]
+  public class Patch
+  {
+    public static void Disable(GrabbableObject item)
+    {
+      if ("LungProp" == item.__getTypeName())
+      {
+        LungProp itemLung = (LungProp)item;
+
+        if (itemLung.GetComponent<AudioSource>().isPlaying)
         {
-            logger = Logger;
-            harmony.PatchAll();
+          Plugin.logger.LogDebug("Disabling sound of LungProp");
 
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+          itemLung.isLungDocked = false;
+          itemLung.isLungDockedInElevator = false;
+          itemLung.isLungPowered = false;
+          itemLung.GetComponent<AudioSource>().Stop();
         }
+      }
     }
 
-    internal class LobbyCompatibilityCompatibility
+    [HarmonyPatch("GrabObjectServerRpc")]
+    [HarmonyPostfix]
+    public static void EquipItemServerRpc(PlayerControllerB __instance, ref NetworkObjectReference grabbedObject)
     {
-        public static void Init()
-        {
-            Plugin.logger.LogWarning("LobbyCompatibility detected, registering plugin with LobbyCompatibility.");
+      if (grabbedObject.TryGet(out var networkObject) && (bool)networkObject.GetComponentInChildren<GrabbableObject>())
+      {
+        Plugin.logger.LogDebug($"EquipItemServerRpc {networkObject.GetComponentInChildren<GrabbableObject>().itemProperties.itemName}");
 
-            Version pluginVersion = Version.Parse(PluginInfo.PLUGIN_VERSION);
-
-            LobbyCompatibility.Features.PluginHelper.RegisterPlugin(PluginInfo.PLUGIN_GUID, pluginVersion, LobbyCompatibility.Enums.CompatibilityLevel.ClientOptional, LobbyCompatibility.Enums.VersionStrictness.None);
-        }
+        Disable(networkObject.GetComponentInChildren<GrabbableObject>());
+      }
     }
 
-    [HarmonyPatch(typeof(PlayerControllerB))]
-    public class Patch
+    [HarmonyPatch("GrabObjectClientRpc")]
+    [HarmonyPostfix]
+    public static void EquipItemClientRpc(PlayerControllerB __instance, ref NetworkObjectReference grabbedObject)
     {
+      if (grabbedObject.TryGet(out var networkObject) && (bool)networkObject.GetComponentInChildren<GrabbableObject>())
+      {
+        Plugin.logger.LogDebug($"EquipItemClientRpc {networkObject.GetComponentInChildren<GrabbableObject>().itemProperties.itemName}");
 
-        public static void Disable(GrabbableObject item)
-        {
-            if ("LungProp" == item.__getTypeName())
-            {
-                LungProp itemLung = (LungProp)item;
-
-                if (itemLung.GetComponent<AudioSource>().isPlaying)
-                {
-                    Plugin.logger.LogDebug("Disabling sound of LungProp");
-
-                    itemLung.isLungDocked = false;
-                    itemLung.isLungDockedInElevator = false;
-                    itemLung.isLungPowered = false;
-                    itemLung.GetComponent<AudioSource>().Stop();
-                }
-            }
-        }
-
-        [HarmonyPatch("GrabObjectServerRpc")]
-        [HarmonyPostfix]
-        public static void EquipItemServerRpc(PlayerControllerB __instance, ref NetworkObjectReference grabbedObject)
-        {
-            if (grabbedObject.TryGet(out var networkObject) && (bool)networkObject.GetComponentInChildren<GrabbableObject>())
-            {
-                Plugin.logger.LogDebug($"EquipItemServerRpc {networkObject.GetComponentInChildren<GrabbableObject>().itemProperties.itemName}");
-
-                Disable(networkObject.GetComponentInChildren<GrabbableObject>());
-            }
-        }
-
-        [HarmonyPatch("GrabObjectClientRpc")]
-        [HarmonyPostfix]
-        public static void EquipItemClientRpc(PlayerControllerB __instance, ref NetworkObjectReference grabbedObject)
-        {
-            if (grabbedObject.TryGet(out var networkObject) && (bool)networkObject.GetComponentInChildren<GrabbableObject>())
-            {
-                Plugin.logger.LogDebug($"EquipItemClientRpc {networkObject.GetComponentInChildren<GrabbableObject>().itemProperties.itemName}");
-
-                Disable(networkObject.GetComponentInChildren<GrabbableObject>());
-            }
-        }
+        Disable(networkObject.GetComponentInChildren<GrabbableObject>());
+      }
     }
+  }
 }
